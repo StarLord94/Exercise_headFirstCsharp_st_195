4cbe5d7d85b1315974964926008851ea394e9c15
Mykola Kuzyk <ukrboy99@gmail.com>
2021-04-18 23:11:04 +0200

    Add project files.

---
+++ Exercise_headFirstCsharp_st_195/Elephant.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise_headFirstCsharp_st_195
+{
+    class Elephant
+    {
+        
+
+       public string Name = "";
+       public int EarSize = 0;
+        
+        public static void StartElephantProgram(Elephant first, Elephant second )
+        {
+            string key;
+            Console.WriteLine("Press 1 for Lloyd, 2 for Lucinda, 3 to swap");
+            key = Console.ReadLine();
+
+            if (key == "1")
+            {
+                first.WhoAmI();
+            }
+
+            else if (key == "2")
+            {
+                second.WhoAmI();
+            }
+            else if (key =="4")
+            {
+                first.SpeakTo(second, "Hi " + second.Name);
+            }
+            else
+            {
+                first.Swap(first,second);
+            }
+        }
+
+        public void WhoAmI()
+        {
+            Console.WriteLine("Hello my name is {0} ", Name);
+            Console.WriteLine("My ears are {0} inches tall ", EarSize);
+
+        }
+
+        public void Swap(Elephant first, Elephant second)
+        {
+             Elephant backToNormal;
+             backToNormal = first;
+             first = second;
+             second = backToNormal;
+
+             second.WhoAmI();
+             first.WhoAmI();
+               
+ 
+            
+        }
+
+        public void HearMessage(string message, Elephant whoSaidIt)
+        {
+            Console.WriteLine(Name + " Heard a message ");
+            Console.WriteLine(whoSaidIt.Name + " said this: " + message);
+
+        }
+        public void SpeakTo(Elephant whoToTalkTo, string message)
+        {
+            whoToTalkTo.HearMessage(message, this);
+
+        }
+
+    }
+}
---
+++ Exercise_headFirstCsharp_st_195/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise_headFirstCsharp_st_195
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elephant Lloid = new Elephant { Name = "Lloyd", EarSize = 40 };
+            Elephant Lucinda = new Elephant { Name = "Lucinda", EarSize = 80 };
+
+            Lloid.HearMessage("Hi", Lucinda );
+            Lucinda.SpeakTo(Lloid,"Hi ");
+
+            int howLong = 0;
+
+            while (howLong == 0)
+            {
+                Elephant BackToNormal = new Elephant { Name = "0", EarSize = 0 };
+
+
+                Elephant.StartElephantProgram(Lloid, Lucinda);
+                Console.WriteLine("Do you want to continiue ?  1 = no, 0 = yes");
+                string howLongSttring = Console.ReadLine();
+                howLong = Int32.Parse(howLongSttring);
+            }
+
+
+
+
+        }
+    }
+}
